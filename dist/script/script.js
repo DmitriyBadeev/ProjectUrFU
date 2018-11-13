42f579758d50e1f29e98bd1fe773118eb37dabf5
Dmitriy Badeev <dmitriibadeev@gmail.com>
2018-11-14 00:34:08 +0500

    fixed bag with icons, add adaptive over 993px

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,21 +1,29 @@
 
- const anchors = [].slice.call(document.querySelectorAll('a[href ="#down"]')),
+ /*const anchors = [].slice.call(document.querySelectorAll('a[href ="#down"]')),
       animationTime = 500,
       framesCount = 100;
 
 anchors.forEach(function(item) {
   item.addEventListener('click', function(e) {
-    e.preventDefault();
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
-    let scroller = setInterval(function() {
 
-      let scrollBy = coordY / framesCount;
-      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        window.scrollBy(0, scrollBy);
-      } else {
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    }, animationTime / framesCount);
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrolled <= 200) {
+        e.preventDefault();
+      let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+      let scroller = setInterval(function() {
+
+        let scrollBy = coordY / framesCount;
+        if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+          window.scrollBy(0, scrollBy);
+        } else {
+          window.scrollTo(0, coordY);
+          clearInterval(scroller);
+        }
+      }, animationTime / framesCount);
+    } else return;
+    
   });
-}); 
+});  */
+
+
