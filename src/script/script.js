a70c8d3a85057308f6c835ce82f690b5c47cf6ea
Dmitriy Badeev <dmitriibadeev@gmail.com>
2018-11-16 12:08:44 +0500

    add adaptiv for md

--- src/script/script.js
+++ src/script/script.js
@@ -27,3 +27,12 @@ anchors.forEach(function(item) {
 });  */
 
 
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrolled > 500) {
+        document.getElementById('up').style.display = 'block';
+    } else {
+      document.getElementById('up').style.display = 'none';
+    }
+  }
